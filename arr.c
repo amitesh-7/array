ca3aabba09e249ce580ea81cab408b6a2fc95a97
Amitesh Vishwakarma <amiteshvishwakarma2006@gmail.com>
2024-11-25 00:43:33 +0530

    New One

---
+++ arr.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    int arr[7];
+    
+    return 0;
+}
---
+++ prime.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, j, prime;
+    printf("Enter a number:\n");
+    scanf("%d", &n);
+    for ( i = 2; i <= n; i++)
+    {
+        prime=1;
+        for ( j = 2; j <i ; j++)
+        {
+            if (i%j==0)
+            {
+                prime=0;
+                break;
+            }
+            
+        }
+        if(prime){
+            printf("%d ", i);
+        }
+    }
+    
+    return 0;
+}
